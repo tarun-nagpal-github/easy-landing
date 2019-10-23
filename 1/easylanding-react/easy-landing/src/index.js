d760c23bf27406a50f73b27b23aa6a8b5870c582
Tarun Nagpal <tarun.nagpal78@gmail.com>
2019-10-23 22:28:44 +0530

    adding package review detail, selecthost.html

--- 1/easylanding-react/easy-landing/src/index.js
+++ 1/easylanding-react/easy-landing/src/index.js
@@ -5,7 +5,7 @@ import Header from './components/global/header/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer  from "./components/global/footer/footer";
 import HomePage from './pages/homepage/homePage';
-import TravelJourney from './pages/TravelJourney/travelJourney'
+// import TravelJourney from './pages/TravelJourney/travelJourney'
 import '../src/common.scss'
 
 
